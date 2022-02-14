a04b37a8b8bbfeb959fb2605700dd8704f8f5894
Bijon Mallik <bijon.ndc.pust@gmail.com>
2022-02-14 12:57:42 +0600

    Testing yargs

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,16 +1,40 @@
 const chalk = require('chalk')
+const yargs = require('yargs')
 
-const error = chalk.bold.red
+const error = chalk.bold.cyan
 const warning = chalk.bold.yellow
 const success = chalk.bold.green
 
-if (process.argv[2]==="add")
-{
-    console.log(success("Adding items"))
-}
-else if(process.argv[2]==="remove")
-{
-    console.log(warning("Removing items"))
-}
+yargs.command({
+    command: "add",
+    description: "Adding item",
+    handler: function(){
+        console.log(error("Adding item in the list"))
+    }
+})
 
+yargs.command({
+    command: "remove",
+    description: "Removing item",
+    handler: function(){
+        console.log(warning("Removing item from the list"))
+    }
+})
 
+yargs.command({
+    command: "read",
+    description: "Reading item",
+    handler: function(){
+        console.log(success("Reading item form the list"))
+    }
+})
+
+yargs.command({
+    command: "list",
+    description: "Showing item",
+    handler: function(){
+        console.log(success("Showing item of the list"))
+    }
+})
+
+console.log(yargs.argv)
