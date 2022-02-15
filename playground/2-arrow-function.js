894f3f4267fca7a1751a392030dc762076ebbf4a
Bijon Mallik <bijon.ndc.pust@gmail.com>
2022-02-15 19:58:27 +0600

    arrow function in object

--- playground/2-arrow-function.js
+++ playground/2-arrow-function.js
@@ -1,11 +1,23 @@
-const squareData = function(x){
-    return x*x
+// const squareData = function(x){
+//     return x*x
+// }
+
+// const cube = (x) =>{
+//     return x*x*x
+// }
+
+// const square = (x) => x*x
+
+// console.log(square(3))
+
+const event ={
+    name : "Birthday Party",
+    guestList : ['Bijon','Eshita','Shakil'],
+    printGuestList(){
+        console.log("Welcome to the "+this.name)
+        this.guestList.forEach((guest)=>{
+            console.log(guest+" has attended!")
+        })
+    }
 }
-
-const cube = (x) =>{
-    return x*x*x
-}
-
-const square = (x) => x*x
-
-console.log(square(3))
+event.printGuestList()
