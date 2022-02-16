a17230f4e495e97b50c86674ff65e9f1f2a897d7
Bijon Mallik <bijon.ndc.pust@gmail.com>
2022-02-16 22:18:52 +0600

    Callback Abstraction Challenge

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,30 +1,12 @@
+const geocode = require('./utils/geocode')
+const forecast = require('./utils/forecast')
 
-const request = require('postman-request')
-
-const url = "http://api.weatherstack.com/current?access_key=2d444274c9bce5cb927c0b36c8bbdf2e&query=37.8267,-122.4233&units=m"
-const geocodeURL = "https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoiYmlqb24xMTYxIiwiYSI6ImNrenBtZGRxbDA1cHQycG16bjc2aXBmMjQifQ.7EIPiv2IRva_DVCKAuAUvA&limit=1"
-
-
-request({ url: url, json: true }, (error, response) => {
-    if (error) {
-        console.log('Unable to connect to weather service!')
-    } else if (response.body.error) {
-        console.log('Unable to find location')
-    } else {
-        console.log(response.body.current.weather_descriptions[0] + ". It is currently " + response.body.current.temperature + " degress out.")
-    }
+geocode('Boston', (error, data) => {
+    console.log('Error', error)
+    console.log('Data', data)
 })
 
-
-
-request({ url: geocodeURL, json: true }, (error, response) => {
-    if (error) {
-        console.log('Unable to connect to location services!')
-    } else if (response.body.features.length === 0) {
-        console.log('Unable to find location. Try another search.')
-    } else {
-        const latitude = response.body.features[0].center[0]
-        const longitude = response.body.features[0].center[1]
-        console.log(latitude, longitude)
-    }
+forecast(44.1545, -75.7088, (error, data) => {
+    console.log('Error', error)
+    console.log('Data', data)
 })
