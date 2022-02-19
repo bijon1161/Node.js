4fddd5aef772fa7940bafbed5098c36d70e35f99
Bijon Mallik <bijon.ndc.pust@gmail.com>
2022-02-19 13:54:09 +0600

    Adding partials

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,28 +1,36 @@
 const path = require('path')
 const express = require('express')
+const hbs = require('hbs')
 
 const app = express()
 const publicDirectoryPath = path.join(__dirname, '../public')
 
 app.set('view engine', 'hbs')
+
+const viewsPath = path.join(__dirname, '../templates/views')
+const partialsPath = path.join(__dirname, '../templates/partials')
+
+app.set('views',viewsPath)
 app.use(express.static(publicDirectoryPath))
+hbs.registerPartials(partialsPath)
 
 app.get('', (req, res) => {
     res.render('index', {
         title: 'Weather',
-        name: 'Andrew Mead'
+        name: 'Bijon'
     })
 })
 
 app.get('/about', (req, res) => {
     res.render('about', {
         title: 'About Me',
-        name: 'Andrew Mead'
+        name: 'Bijon'
     })
 })
 
 app.get('/help', (req, res) => {
     res.render('help', {
+        name: 'Bijon',
         helpText: 'This is some helpful text.'
     })
 })
