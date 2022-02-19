a57dcc3dcfc330e5c88ec214c207b640356977cb
Bijon Mallik <bijon.ndc.pust@gmail.com>
2022-02-19 17:25:52 +0600

    Query string

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -3,42 +3,70 @@ const express = require('express')
 const hbs = require('hbs')
 
 const app = express()
+
+// Define paths for Express config
 const publicDirectoryPath = path.join(__dirname, '../public')
-
-app.set('view engine', 'hbs')
-
 const viewsPath = path.join(__dirname, '../templates/views')
 const partialsPath = path.join(__dirname, '../templates/partials')
 
-app.set('views',viewsPath)
-app.use(express.static(publicDirectoryPath))
+// Setup handlebars engine and views location
+app.set('view engine', 'hbs')
+app.set('views', viewsPath)
 hbs.registerPartials(partialsPath)
+
+// Setup static directory to serve
+app.use(express.static(publicDirectoryPath))
 
 app.get('', (req, res) => {
     res.render('index', {
         title: 'Weather',
-        name: 'Bijon'
+        name: 'Bijon Mallik'
     })
 })
 
 app.get('/about', (req, res) => {
     res.render('about', {
         title: 'About Me',
-        name: 'Bijon'
+        name: 'Bijon Mallik'
     })
 })
 
 app.get('/help', (req, res) => {
     res.render('help', {
-        name: 'Bijon',
-        helpText: 'This is some helpful text.'
+        helpText: 'This is some helpful text.',
+        title: 'Help',
+        name: 'Bijon Mallik'
     })
 })
 
 app.get('/weather', (req, res) => {
+    console.log(req.query)
+    if (!req.query.address) {
+        return res.send({
+            error: 'You must provide an address!'
+        })
+    }
+
     res.send({
-        forecast: 'It is snowing',
-        location: 'Philadelphia'
+        forecast: 'It is raining',
+        location: 'Bangladesh',
+        address: req.query.address
+    })
+})
+
+app.get('/help/*', (req, res) => {
+    res.render('404', {
+        title: '404',
+        name: 'Bijon Mallik',
+        errorMessage: 'Help article not found.'
+    })
+})
+
+app.get('*', (req, res) => {
+    res.render('404', {
+        title: '404',
+        name: 'Bijon Mallik',
+        errorMessage: 'Page not found.'
     })
 })
 
